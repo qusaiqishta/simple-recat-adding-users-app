14aaa3db9270df9f02c11165772d0e0dbe02bfe0
Qusai Qishta <Qusai.Qishta@qusais-macbook-pro.home>
2022-12-10 15:41:20 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React,{useState} from 'react';
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UserList';
+function App() {
+const [users,setUsers]=useState([]);
+const addUserHandler=(uName,uAge)=>{
+  setUsers(prevState=>{
+    return [...prevState,{name:uName,age:uAge,key:Math.random()}]
+  })
+}
+  return (
+    <div>
+  <AddUser onAddUser={addUserHandler}/>
+  <UsersList users={users}/>
+    </div>
+  );
+}
+
+export default App;
